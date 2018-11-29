9b1d4b87dbff00b3b90b015bddbea6b442cdebd1
Daomephsta <dtnilfisk@gmail.com>
2018-11-29 20:51:17 +1300

    Move texture creation from file into ResourceManager

--- GLObjects/Texture.cs
+++ GLObjects/Texture.cs
@@ -8,32 +8,19 @@ using log4net;
 
 namespace Tangerine.GLObjects
 {
-    public struct Texture
+    public class Texture
     {
         private static readonly ILog LOGGER = LogManager.GetLogger(typeof(Texture));
         internal readonly int glID;
 
-        public Texture(String fileName) : this(fileName, imaging::PixelFormat.Format24bppRgb, opengl::PixelFormat.Bgr) { }
-
-        public Texture(String fileName, imaging::PixelFormat imageFormat, opengl::PixelFormat format)
+        public Texture(opengl::PixelFormat format, int width, int height, IntPtr pixels)
         {
             glID = GL.GenTexture();
-            LOGGER.Debug($"Loading texture from {fileName} into GL texture {glID} Image Format: {imageFormat} Pixel Format: {format}");
-            using (Bitmap bitmap = new Bitmap(fileName))
-            {
-                bitmap.RotateFlip(RotateFlipType.RotateNoneFlipY);
-                BitmapData imageData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, imageFormat);
-
-                GL.BindTexture(TextureTarget.Texture2D, glID);
-
-                GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, bitmap.Width, bitmap.Height, 0, format, PixelType.UnsignedByte, imageData.Scan0);
-                GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
-
-                //Cleanup
-                GL.BindTexture(TextureTarget.Texture2D, 0);
-                bitmap.UnlockBits(imageData);
-            }
-            LOGGER.Info($"Loaded texture from {fileName}");
+            GL.BindTexture(TextureTarget.Texture2D, glID);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, width, height, 0, format, PixelType.UnsignedByte, pixels);
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+            //Cleanup
+            GL.BindTexture(TextureTarget.Texture2D, 0);
         }
 
         public void Bind()
--- ResourceManager.cs
+++ ResourceManager.cs
@@ -2,7 +2,11 @@
 using System;
 using System.IO;
 using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
 using Tangerine.GLObjects;
+using opengl = OpenTK.Graphics.OpenGL;
+using imaging = System.Drawing.Imaging;
 
 namespace Tangerine
 {
@@ -20,6 +24,23 @@ namespace Tangerine
                 }
             }
             return new Shader(type, shaderPath, source.ToString());
+        }
+
+        public static Texture CreateTextureFromFile(String filePath)
+        {
+            return CreateTextureFromFile(filePath, imaging::PixelFormat.Format24bppRgb, opengl::PixelFormat.Bgr);
+        }
+
+        public static Texture CreateTextureFromFile(String filePath, imaging::PixelFormat imageFormat, opengl::PixelFormat pixelFormat)
+        {
+            using (Bitmap bitmap = new Bitmap(filePath))
+            {
+                bitmap.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                BitmapData imageData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, imageFormat);
+                bitmap.UnlockBits(imageData);
+
+                return new Texture(pixelFormat, bitmap.Width, bitmap.Height, imageData.Scan0);
+            }
         }
     }
 }
